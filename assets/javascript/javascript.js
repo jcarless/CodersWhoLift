522e8779d40a28293ce4124368f23033453c3b31
Jordan Biason <JordanB@nbp-25-119.nbp.ruw.rutgers.edu>
2016-03-16 20:56:49 -0400

    updated

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,22 @@
+jQuery(document).ready(function($){
 function generateExercise(){
 
+	// var q = $(this).data('image');
+
+	// var squatsmuscles = "assets/images/squatsmuscles.png"
+	// var rowsmuscles = "assets/images/rowmuscles.png"
+	// var deadmuscles = "assets/images/deadliftmuscles.png"
+	// var benchmuscles = "assets/images/benchmuscles.png"
+	// var militarymuscles = "assets/images/militarymuscles.png"
+	// var crunchesmuscles = "assets/images/crunchesmuscles.png"
+
+	// var muscles = ["squatsmuscles", "rowmuscles", "deadmuscles", "benchmuscles", "militarymuscles", "crunchesmuscles"];
+
+
+
+
+
+// APPENDS DESCRIPTION TO PAGE
 	var i = $(this).data('id');
 
 	var queryURL = "https://wger.de/api/v2/exercise/"+i+"/?format=json";
@@ -19,9 +36,11 @@ function generateExercise(){
 
 		$('.description').append(response.description);
 
+	
 
 	})
 
+		// $(".diagram").append("<img src=>");
 }
 
 $('#squats').on('click', generateExercise);
@@ -30,3 +49,5 @@ $('#benchpress').on('click', generateExercise);
 $('#militarypress').on('click', generateExercise);
 $('#bentoverrows').on('click', generateExercise);
 $('#crunches').on('click', generateExercise);
+
+})
