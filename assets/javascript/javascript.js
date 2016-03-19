525f401a9529678e2f753983b9fed9d034921d75
Jordan Biason <JordanB@nbp-137-124.nbp.ruw.rutgers.edu>
2016-03-19 11:33:57 -0400

    hopefully this works

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,16 +3,16 @@ function generateExercise(){
 
 	var q = $(this).data('image');
 
-	var squatsmuscles = "assets/images/squatsmuscles.png"
-	var rowsmuscles = "assets/images/rowmuscles.png"
-	var deadmuscles = "assets/images/deadliftmuscles.png"
-	var benchmuscles = "assets/images/benchmuscles.png"
-	var militarymuscles = "assets/images/militarymuscles.png"
-	var crunchesmuscles = "assets/images/crunchesmuscles.png"
+		var squatsmuscles = 'assets/images/squatsmuscles.png'
+		var rowsmuscles = 'assets/images/rowmuscles.png'
+		var deadmuscles = 'assets/images/deadliftmuscles.png'
+		var benchmuscles = 'assets/images/benchmuscles.png'
+		var militarymuscles = 'assets/images/militarymuscles.png'
+		var crunchesmuscles = 'assets/images/crunchesmuscles.png'
 
 	var muscles = [squatsmuscles, rowsmuscles, deadmuscles, benchmuscles, militarymuscles, crunchesmuscles];
 
-	$('.diagram').append("<img src='"+muscles[q]+"'>");
+
 
 
 // APPENDS DESCRIPTION TO PAGE
@@ -27,15 +27,15 @@ function generateExercise(){
 
 	.done(function(response){
 		var results = response;
-		$('.muscles').empty();
+		$('.diagram').empty();
 		$('.description').empty();
-		$('.youtubeArea').empty();
+
 
 		console.log(results);
 
 
 		$('.description').append(response.description);
-		$('.muscles').append("<img src='"+muscles[q]+"'width='450px' height='350px'>")
+		$('.diagram').append("<img src='"+muscles[q]+"'width='450px' height='350px'>")
 
 	})
 
@@ -49,3 +49,125 @@ $('#bentoverrows').on('click', generateExercise);
 $('#crunches').on('click', generateExercise);
 
 })
+
+
+// ------FIREBASE---------------
+
+var fitData = new Firebase("https://thefitnessapp.firebaseio.com");
+
+// Search for User
+$('#login').on('click', function(){
+	console.log('testing');
+	// Grab Text box value 
+	var textbox = $('#userID').val().trim(); // TEXTBOX VALUE REPLACE
+
+
+	// Print info 
+	fitData.on("child_added", function(snapshot) {
+
+		
+
+		// If the snapshot.key is equal to the textbox value 
+		if (snapshot.key() === textbox) {
+	     
+
+			$('#placeholderChart').empty();
+
+	    var userReps = snapshot.val().repData;
+		var userSets = snapshot.val().setData;
+		var userWeight = snapshot.val().weightData;
+		var name = snapshot.key();
+
+		// Google charts-----------------------------
+
+ google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Week', 'Reps', 'Sets', 'Weight (x10)'],
+          ['Jan 1',  10,      4,	15],
+          ['Jan 7',  12,      4,	16],
+          ['Jan 14',  8,       3,	18],
+          ['Jan 21',  10,      4,	21],
+          ['Feb 1',  12,      5,	25],
+          ['Feb 7',  15,      5,	31],
+          ['Feb 7',  12,      4,	25],
+          ['Feb 7',  10,      3,	19]
+        ]);
+
+        var options = {
+          title: 'Workout Performance',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      } //chart
+	      
+
+			console.log(snapshot.key() + " did " + userReps + " reps and " + userSets + ' sets at ' + userWeight + ' lbs');
+
+			$("#addWorkout").on("click", function(){
+	
+			// Grab Text box value 
+			var reps = $('#reps').val().trim();
+			var sets = $('#sets').val().trim();
+			var weight = $('#weight').val().trim();
+
+
+			// PUSH with correct key name
+			fitData.child(textbox).set({
+				repData: reps,
+				setData: sets,
+				weightData: weight
+				});
+
+
+			$('#stats').html("<tr>"+
+						"<td>"+reps+"</td>"+
+						"<td>"+sets+"</td>"+
+						"<td>"+weight+"</td>"+
+						+"</tr>");
+
+			$('#reps').val('');
+			$('#sets').val('');
+			$('#weight').val('');
+
+			
+			return false;
+		});	//addWorkout button
+
+			
+
+	}; //if function
+
+
+
+}) //on child_added
+return false;
+}) //login button
+
+
+// Generates YouTube video section
+
+
+	$('.video').on('click', function(){
+	$('.youtubeArea').empty();
+	var p = $(this).data('playlist');
+	var playlistID = p;
+		APIkey = "AIzaSyBtMXG8W1P4x4ruQm8r8TjNX1gEjLDWOdo";
+		baseURL = "https://www.googleapis.com/youtube/v3/";
+	var queryURL = baseURL + "playlistItems?part=snippet&maxResults=10&playlistId=" + playlistID + "&key=" + APIkey + "/";
+
+		 $.ajax({
+		 	url: queryURL,
+		 	method: "GET"
+		 })
+
+
+	$('.youtubeArea').append("<iframe width='560' height='315' src='https://www.youtube.com/embed/videoseries?list="+playlistID+"' frameborder='0' allowfullscreen></iframe>");
+});
+
