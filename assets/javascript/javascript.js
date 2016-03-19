e499ae82d3c016435bf51903cfdee7f4f6415f5f
Jordan Biason <JordanB@nbp-137-124.nbp.ruw.rutgers.edu>
2016-03-19 14:06:58 -0400

    something something

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -61,34 +61,108 @@ $('#crunches').on('click', generateExercise);
 })
 
 
-// ------FIREBASE---------------
+// ------FIREBASE----------------------------------------
 
 var fitData = new Firebase("https://thefitnessapp.firebaseio.com");
 
+// Grab Text box value 
+var userName = $('#userID').val().trim(); // userName VALUE REPLACE
+
 // Search for User
 $('#login').on('click', function(){
-	console.log('testing');
+	
 	// Grab Text box value 
-	var textbox = $('#userID').val().trim(); // TEXTBOX VALUE REPLACE
+	userName = $('#userID').val().trim(); // userName VALUE REPLACE
+
+	// firebase api
+	var queryURL = 'https://thefitnessapp.firebaseio.com/'+ userName +'.json';
+
+	$.ajax({url: queryURL, method: 'GET'})
+		.done(function(response) {
+
+	var reps1 = response.repData;
+	var sets1 = response.setData;
+	var weight1 = response.weightData;
+
+	console.log(reps1);
+	console.log(sets1);
+	console.log(weight1);
 
 
-	// Print info 
-	fitData.on("child_added", function(snapshot) {
+
+		}) //.done
+
+
+
+
+// 	// Print info 
+// 	fitData.on("child_added", function(snapshot) {
 
 		
 
-		// If the snapshot.key is equal to the textbox value 
-		if (snapshot.key() === textbox) {
+// 		// If the snapshot.key is equal to the userName value 
+// 		if (snapshot.key() === userName) {
 	     
+// 	    var userReps = snapshot.val().repData;
+// 		var userSets = snapshot.val().setData;
+// 		var userWeight = snapshot.val().weightData;
+// 		var name = snapshot.key();
 
-			$('#placeholderChart').empty();
+// 		// Google charts-----------------------------
 
-	    var userReps = snapshot.val().repData;
-		var userSets = snapshot.val().setData;
-		var userWeight = snapshot.val().weightData;
-		var name = snapshot.key();
 
-		// Google charts-----------------------------
+	      
+
+// 			console.log(snapshot.key() + " did " + userReps + " reps and " + userSets + ' sets at ' + userWeight + ' lbs');
+
+
+
+			
+
+// 	}; //if function
+
+
+
+// }) //on child_added
+return false;
+}) //login button
+
+$("#addWorkout").on("click", function(){
+	
+			// Grab Text box value 
+			var reps = $('#reps').val().trim();
+			var sets = $('#sets').val().trim();
+			var weight = $('#weight').val().trim();
+
+
+			// PUSH with correct key name
+			// console.log(userName);
+			// console.log(reps);
+			// console.log(sets);
+			// console.log(weight);
+
+			var currentDateTime = moment().format("X");
+
+			fitData.child(userName).child(currentDateTime).set({
+				repData: reps,
+				setData: sets,
+				weightData: weight
+				});
+
+
+			// $('#stats').html("<tr>"+
+			// 			"<td>"+reps+"</td>"+
+			// 			"<td>"+sets+"</td>"+
+			// 			"<td>"+weight+"</td>"+
+			// 			+"</tr>");
+
+			// $('#reps').val('');
+			// $('#sets').val('');
+			// $('#weight').val('');
+
+			
+			return false;
+		});	//addWorkout button
 
  google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
@@ -116,49 +190,7 @@ $('#login').on('click', function(){
 
         chart.draw(data, options);
       } //chart
-	      
-
-			console.log(snapshot.key() + " did " + userReps + " reps and " + userSets + ' sets at ' + userWeight + ' lbs');
-
-			$("#addWorkout").on("click", function(){
-	
-			// Grab Text box value 
-			var reps = $('#reps').val().trim();
-			var sets = $('#sets').val().trim();
-			var weight = $('#weight').val().trim();
-
-
-			// PUSH with correct key name
-			fitData.child(textbox).set({
-				repData: reps,
-				setData: sets,
-				weightData: weight
-				});
-
-
-			$('#stats').html("<tr>"+
-						"<td>"+reps+"</td>"+
-						"<td>"+sets+"</td>"+
-						"<td>"+weight+"</td>"+
-						+"</tr>");
-
-			$('#reps').val('');
-			$('#sets').val('');
-			$('#weight').val('');
-
-			
-			return false;
-		});	//addWorkout button
-
-			
-
-	}; //if function
-
-
-
-}) //on child_added
-return false;
-}) //login button
+// -----------------FIREBASE-------------------------------------------
 
 
 // Generates YouTube video section
